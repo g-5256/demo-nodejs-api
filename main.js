02d88b1be1ffad2baa28dfdd156d8257066b6e8e
g-5256 <121560820+g-5256@users.noreply.github.com>
2023-06-12 12:34:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,4 @@
 
-// const http = require ('http');
-
-// const payload = {
-//   "name" : "Gaurav Suryavanshi" ,
-//   "email" : "gauravsuryavanshi.dev@gmail.com",
-//   "roll_no" : "TECOMPB39"
-// }
-
-// const post_options = {
-//   hostname : 'postman-echo.com',
-//   method : "POST",
-//   path : "/post",
-//   headers :{
-//     "Content-Type" : "application/json"
-//   },
-// }
-
-// const post_request = http.request (post_options , (response) => 
-// {
-//   console.log('Response Code: ${response.statusCode}');
-//   console.log('Response Body:');
-//   response.pipe(process.stdout);
-// }
-// )
-
-// // post_request.on('error' , (err) => {})
-
-// post_request.end(payload);
 const http = require('http');
 
 const payload = {
